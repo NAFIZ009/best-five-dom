c82567e11078c0fed1cb214175077547b4bb97c9
NAFIZ009 <jalalnafiz3@gmail.com>
2022-08-22 17:10:14 +0600

    forth

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 let playerName=[];
+//fun for select player
 function selectPlayer(e) {
     playerName.unshift(e.target.parentElement.children[0].innerText);
 }
+//adding player
 [...document.getElementsByClassName('player-selection-btn')].forEach(element => {
     element.addEventListener('click', (e) => {
         selectPlayer(e);
         addPlayerName(e);
     });
 });
+//add player fun
 function addPlayerName(e) {
     let parent=document.getElementById('player-container');
     if([...parent.children].some(element => element.getAttribute('id')=="noticeOfList")){
@@ -24,9 +27,42 @@ function addPlayerName(e) {
     }
 
 }
+//calculate player price
+document.getElementById("priceCalBtn").addEventListener("click", function(e){
+    playerPriceCal();
+});
+document.getElementById("totalPriceCalBtn").addEventListener("click", function(e){
+    subTotal();
+});
 function playerPriceCal() {
-    // if(typeof document.getElementById('playerPriceIndex')=='string'){
-    //     alert('give a valid price in')
-    // }
-    document.getElementById('priceRes').innerText=parseInt()
+    if(document.getElementById('playerPriceIndex').value.match(/^[a-zA-Z]+$/)){
+        alert('give a valid price');
+        document.getElementById('playerPriceIndex').value='';
+    }else{
+        let parentElement = document.getElementById('priceRes');
+        let ammount=parseInt(document.getElementById("playerPriceIndex").value)*playerName.length;
+        parentElement.innerText = ammount;
+    }
+
+}
+function subTotal() {
+    let playerPriceValue=document.getElementById('priceRes').innerText;
+    console.log(playerPriceValue);
+    let managerPrice=parseInt(document.getElementById("managerPrice").value);
+    let coachPrice=parseInt(document.getElementById("coachPrice").value);
+    if(document.getElementById("managerPrice").value.match(/^[a-zA-Z]+$/) || document.getElementById("coachPrice").value.match(/^[a-zA-Z]+$/)){
+        alert('give a valid price');
+        document.getElementById("managerPrice").value='';
+        document.getElementById("coachPrice").value='';
+    }else{
+        if(playerPriceValue==""){
+            let subTotal=managerPrice+coachPrice;
+            document.getElementById("totalPriceRes").innerText=subTotal;
+        }else{
+            let subTotal=managerPrice+coachPrice+parseInt(playerPriceValue);
+            document.getElementById("totalPriceRes").innerText=subTotal;
+        }
+        
+    }
+
 }
